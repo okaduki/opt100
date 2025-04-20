c923b11ceb587bb5c57db37ebf29809484d76098
okaduki <okaduki1@gmail.com>
2025-04-20 13:46:45 +0900

    impl dual bound

--- didpy/mysolver.py
+++ didpy/mysolver.py
@@ -60,10 +60,12 @@ class DpJspSolver(DpSolver, WarmstartMixin):
         # dp[U][t][m1][m2]...[j1][j2]... := 未処理のタスク集合U, ジョブの最遅終了時刻t, M1 の終了時刻がm1, ..., ジョブ1の終了時刻がj1, ... のときのmakespan の最小値
         # dp[U,t,m1,..., j1,...] =
         #   min_{J_i_j \in U, J_i_{j-1} \not\in U}(
-        #       t' - t + dp[U \ J_i_j, t', m1, ..., m_p + C_{i,j}, ..., j1, ..., j_j + C_{i,j}, ...)
+        #       t' - t + dp[U \ J_i_j, t', m1, ..., m'_p, ..., j1, ..., j'_j, ...)
         #   )
         #   where
         #       p = machine[i][j]
+        #       m'_p = max(m_p + C_{i,j}, j_j + C_{i,j})
+        #       j'_j = max(m_p + C_{i,j}, j_j + C_{i,j})
         #       t' = max(t, m_p + C_{i,j}, j_j + C_{i,j})
 
         finish = model.add_int_var(0)
@@ -188,16 +190,18 @@ class DpJspSolver2(DpSolver, WarmstartMixin):
     problem: JobShopProblem
 
     def init_model(self, **kwargs: Any) -> None:
+        n_jobs = self.problem.n_jobs
         model = dp.Model()
         jobs = []
         durations = []
         machines = []
         job_id = []
-        cur_sub_job_per_jobs = {i: 0 for i in range(self.problem.n_jobs)}
+        cur_sub_job_per_jobs = {i: 0 for i in range(n_jobs)}
         index = {}
         len_ = 0
+
         while len_ < self.problem.n_all_jobs:
-            for i in range(self.problem.n_jobs):
+            for i in range(n_jobs):
                 if cur_sub_job_per_jobs[i] < len(self.problem.list_jobs[i]):
                     jobs.append((i, cur_sub_job_per_jobs[i]))
                     durations.append(
@@ -213,7 +217,7 @@ class DpJspSolver2(DpSolver, WarmstartMixin):
                     cur_sub_job_per_jobs[i] += 1
                     len_ += 1
         precedence_by_index = [set() for i in range(self.problem.n_all_jobs)]
-        for i in range(self.problem.n_jobs):
+        for i in range(n_jobs):
             for j in range(1, len(self.problem.list_jobs[i])):
                 ind = index[(i, j)]
                 ind_pred = index[(i, j - 1)]
@@ -233,37 +237,39 @@ class DpJspSolver2(DpSolver, WarmstartMixin):
         ]
         cur_time_per_job = [
             model.add_int_resource_var(target=0, less_is_better=True)
-            for m in range(self.problem.n_jobs)
+            for m in range(n_jobs)
         ]
 
         next_task_per_job = [
-            model.add_element_var(object_type=subtask, target=0)
-            for m in range(self.problem.n_jobs)
+            model.add_element_resource_var(
+                object_type=subtask, target=0, less_is_better=False
+            )
+            for m in range(n_jobs)
         ]
+
+        job_bounds = []
+        for i in range(n_jobs):
+            arr = [0] * (n_subjob + 1)
+            for j in range(n_subjob - 1, -1, -1):
+                arr[j] = arr[j + 1] + self.problem.list_jobs[i][j].processing_time
+            table = model.add_int_table(arr)
+            job_bounds.append(table)
 
         # dp[l1][l2]...[t][m1][m2]...[j1][j2]... := ジョブ1 の次のタスクがl1,..., ジョブの最遅終了時刻t, M1 の終了時刻がm1, ..., ジョブ1の終了時刻がj1, ... のときのmakespan の最小値
         # dp[l1,...,t,m1,..., j1,...] =
         #   min_{J_{i,j} | l_i == j }(
-        #       t' - t + dp[..., l_i+1, ..., t', m1, ..., m_p + C_{i,j}, ..., j1, ..., j_j + C_{i,j}, ...)
+        #       t' - t + dp[..., l_i+1, ..., t', m1, ..., m'_p, ..., j1, ..., j'_j, ...)
         #   )
         #   where
         #       p = machine[i][j]
-        #       t' = max(t, m_p + C_{i,j}, j_j + C_{i,j})
-
-        # dual bound for dp[U] = \max_{0 <= i < n} \sum_{0 <= j < m, J_{i,j} \in U} p_{i,j}
-        # table = model.add_int_table([
-        #     durations[i] for i in range(len(jobs))
-        # ])
-        # model.add_dual_bound(table[undone])
+        #       end = max(m_p + C_{i,j}, j_j + C_{i,j})
+        #       m'_p = end
+        #       j'_j = end
+        #       t' = max(t, end)
 
         finish = model.add_int_var(0)
         cur_time_total = model.add_int_resource_var(target=0, less_is_better=True)
-        model.add_base_case(
-            [
-                finish == 1,
-                sum(next_task_per_job) == self.problem.n_jobs * n_subjob,
-            ]
-        )
+        model.add_base_case([finish == 1])
         self.transitions = {}
         for i in range(len(jobs)):
             m = machines[i]
@@ -271,42 +277,19 @@ class DpJspSolver2(DpSolver, WarmstartMixin):
             jid = job_id[i]
             subjob_id = jobs[i][1]
 
+            end_time = dp.max(
+                cur_time_per_machine[m] + dur, cur_time_per_job[jid] + dur
+            )
+            nt = dp.max(cur_time_total, end_time)
+
             sched = dp.Transition(
                 name=f"sched_{i}",
-                cost=(
-                    dp.max(
-                        cur_time_total,
-                        dp.max(
-                            cur_time_per_machine[m] + dur, cur_time_per_job[jid] + dur
-                        ),
-                    )
-                    - cur_time_total
-                )
-                + dp.IntExpr.state_cost(),
+                cost=nt - cur_time_total + dp.IntExpr.state_cost(),
                 effects=[
                     (next_task_per_job[jid], next_task_per_job[jid] + 1),
-                    (
-                        cur_time_per_job[jid],
-                        dp.max(
-                            cur_time_per_machine[m] + dur, cur_time_per_job[jid] + dur
-                        ),
-                    ),
-                    (
-                        cur_time_per_machine[m],
-                        dp.max(
-                            cur_time_per_machine[m] + dur, cur_time_per_job[jid] + dur
-                        ),
-                    ),
-                    (
-                        cur_time_total,
-                        dp.max(
-                            cur_time_total,
-                            dp.max(
-                                cur_time_per_machine[m] + dur,
-                                cur_time_per_job[jid] + dur,
-                            ),
-                        ),
-                    ),
+                    (cur_time_per_job[jid], end_time),
+                    (cur_time_per_machine[m], end_time),
+                    (cur_time_total, nt),
                 ],
                 preconditions=[next_task_per_job[jid] == subjob_id],
             )
@@ -318,10 +301,27 @@ class DpJspSolver2(DpSolver, WarmstartMixin):
             cost=dp.IntExpr.state_cost(),
             preconditions=[
                 finish == 0,
-                sum(next_task_per_job) == self.problem.n_jobs * n_subjob,
+                sum(next_task_per_job) == n_jobs * n_subjob,
             ],
         )
-        model.add_transition(finish_transition)
+        model.add_transition(finish_transition, forced=True)
+
+        # dual bound for dp[l1,...] = \max_{0 <= i < n} \sum_{l_i <= j < m} p_{i,j}
+        if False:
+            expr = job_bounds[0][next_task_per_job[0]]
+            for jid in range(1, self.problem.n_jobs):
+                expr = dp.max(expr, job_bounds[jid][next_task_per_job[jid]])
+            model.add_dual_bound(expr)
+        else:  # faster 7 secs
+            l = 1
+            exprs = [job_bounds[jid][next_task_per_job[jid]] for jid in range(n_jobs)]
+            while l < n_jobs:
+                for jid in range(0, n_jobs, l * 2):
+                    if jid + l < n_jobs:
+                        exprs[jid] = dp.max(exprs[jid], exprs[jid + l])
+                l *= 2
+            model.add_dual_bound(exprs[0])
+
         self.transitions["finish"] = finish_transition
         self.jobs = jobs
         self.prec = precedence_by_index
